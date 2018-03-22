61b2b8556043b945e3f7c53113d4c923cb80a3d2
Тетерин Святослав <svyatoslavteterin@gmail.com>
2018-03-22 13:39:10 +0300

    fix viewport bug

--- app/js/app.js
+++ app/js/app.js
@@ -14,3 +14,12 @@
  */
 
 //= lodash.js
+
+
+
+
+document.querySelector('.basket-btn').addEventListener('click',function(e){
+    e.preventDefault();
+    document.getElementById('mini-cart').style.display="block";
+
+});
