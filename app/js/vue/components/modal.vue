b8a660b40685d3a1afb496530f564db043dc3de5
Тетерин Святослав <svyatoslavteterin@gmail.com>
2018-06-28 22:45:32 +0300

    add app

---
+++ app/js/vue/components/modal.vue
@@ -0,0 +1,24 @@
+<template>
+    <transition name="modal">
+        <div class="modal-mask">
+            <div class="modal-wrapper">
+                <div class="modal-container">
+                    <a class="modal-close"  @click="$emit('close')">x</a>
+                    <div class="modal-header">
+                        <slot name="header">
+                            default header
+                        </slot>
+                    </div>
+
+                    <div class="modal-body">
+                        <slot name="body">
+                            default body
+                        </slot>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </transition>
+</template>
---
+++ app/js/vue/components/search.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="search-input">
+        <input type="text" placeholder="Поиск" v-model="query"  />
+    </div>
+</template>
+
+<script>
+    module.exports = {
+        methods: {
+
+
+        },
+        created() {
+
+        },
+        data() {
+            return {
+                q:''
+            }
+        },
+
+        props: ['data'],
+
+        ready: function () {
+
+        },
+        mounted: function () {
+
+        },
+        computed: {
+            query: {
+                get:function(){
+                    return store.state.q;
+                },
+                set:function(newValue){
+                    store.commit('changeSearchQuery',{"value":newValue});
+
+                }
+            }
+        }
+    }
+</script>
