0476e0e2c8a5ce84c9eeb7e60803829bc2bc2c95
Тетерин Святослав <svyatoslavteterin@gmail.com>
2018-06-28 22:08:56 +0300

    Авторизация и Регистрация

--- app/js/app.js
+++ app/js/app.js
@@ -3,7 +3,7 @@
 
 window._ = require('lodash');
 
-window.Popper=require('popper.js')
+window.Popper = require('popper.js')
 
 
 /* Vue */
@@ -16,6 +16,10 @@ Vue.config.debug = true;
 
 import Vuex from 'vuex'
 import axios from 'axios'
+
+import VModal from 'vue-js-modal'
+
+Vue.use(VModal)
 
 Vue.use(Vuex);
 
@@ -31,34 +35,35 @@ Vue.prototype.$http = axios;
 window.store = new Vuex.Store({
     state: {
         cart: [],
-        area:2,
-        filters:[],
-        q:''
+        area: 2,
+        filters: [],
+        q: '',
+        authUser:{}
     },
     mutations: {
-        loadFoods:function(state,payload){
-          state.foods=payload.value;
+        loadFoods: function (state, payload) {
+            state.foods = payload.value;
         },
-        addToCart:function(state,payload){
+        addToCart: function (state, payload) {
             state.cart.push(payload.value);
         },
-        changeArea:function(state,payload){
-            state.area=payload.value;
+        changeArea: function (state, payload) {
+            state.area = payload.value;
         },
-        addFilter:function(state,payload){
+        addFilter: function (state, payload) {
             state.filters.push(payload.value);
         },
-        changeSearchQuery:function(state,payload){
-            state.q=payload.value;
+        changeSearchQuery: function (state, payload) {
+            state.q = payload.value;
+        },
+        setAuthUser:function(state,payload){
+            state.authUser=payload.value;
         }
     },
-    getters: {
+    getters: {},
+    actions: {
 
-
-
-
-    },
-    actions: {}
+    }
 
 });
 
@@ -69,8 +74,8 @@ window.BurgerApp = new Vue({
 
     data: {
         "menu": [],
-        "ready":false,
-        "filters":['Веганам','С рыбой','С говядиной','С курицей','С индейкой','С морепродуктами'],
+        "ready": false,
+        "filters": ['Веганам', 'С рыбой', 'С говядиной', 'С курицей', 'С индейкой', 'С морепродуктами'],
     },
     watch: {
         show(val) {
@@ -84,29 +89,89 @@ window.BurgerApp = new Vue({
 
     },
     methods: {
+        restorePassword:function(){
+
+        },
+        showRegister:function(){
+            this.$modal.hide('login');
+            this.$modal.show('register');
+        },
+        auth:function(){
+            let formData = new FormData(document.querySelector('#auth-form'));
+            let data={};
+            data.username=formData.get('username');
+            data.password=formData.get('password');
+
+            this.$http.post('http://apitest.burgerpizzoni.ru/api/Profiles/login',data).then((response)=>{
+                console.log(response.data);
+                this.$modal.hide('login');
+                store.commit('setAuthUser',{'value':response.data});
+            }).catch((error)=>{console.log(error)});
+        },
+        register:function(){
+            let formData = new FormData(document.querySelector('#reg-form'));
+            let data={};
+            data.name=formData.get('name');
+            data.phone=formData.get('phone');
+
+            this.$http.post('http://apitest.burgerpizzoni.ru/api/Profiles/regStep1',data).then((response)=>{
+                console.log(response.data);
+                this.$modal.hide('register');
+                this.$modal.show('register2');
+            }).catch((error)=>{console.log(error)});
+        },
+        activate:function(){
+            let formData = new FormData(document.querySelector('#reg-form2'));
+            let data={};
+            data.code=formData.get('code');
+            data.phone=formData.get('phone');
+            this.$http.post('http://apitest.burgerpizzoni.ru/api/Profiles/regStep2',data).then((response)=>{
+                console.log(response.data);
+                this.$modal.hide('register2');
+                store.commit('setAuthUser',{'value':response.data});
+
+            }).catch((error)=>{console.log(error)});
+        },
         beforeOpen: function () {
 
         },
         beforeClose: function () {
 
+        },
+        show(modalName) {
+            this.$modal.show(modalName);
+        },
+        hide(modalName) {
+            this.$modal.hide(modalName);
         }
-
-
     },
     created: function () {
 
 
     },
     computed: {
-        currentArea:function(){
+        checkLogin:function(){
+            if (Object.keys(store.state.authUser).length === 0 && store.state.authUser.constructor === Object){
+                return 0;
+            }else{
+                return 1;
+            }
+
+        },
+        userInfo:function(){
+            return store.state.authUser.userInfo.FirstName;
+        },
+
+
+        currentArea: function () {
             return store.state.area;
         },
-        foods:function(){
-            if (store.state.q){
+        foods: function () {
+            if (store.state.q) {
 
-                    return this.menu[store.state.area].categs.filter(item =>
-                        item.categName.toLowerCase().indexOf(store.state.q) > 0
-                    );
+                return this.menu[store.state.area].categs.filter(item =>
+                    item.categName.toLowerCase().indexOf(store.state.q) > 0
+                );
 
             }
             return this.menu[store.state.area].categs;
@@ -118,7 +183,7 @@ window.BurgerApp = new Vue({
         this.$http.get('http://89.223.25.82:3030/api/menu/getMenuFront').then((response) => {
             this.menu = response.data.menu;
 
-            this.ready=true;
+            this.ready = true;
 
 
         }, (response) => {
@@ -131,9 +196,7 @@ window.BurgerApp = new Vue({
 });
 
 
-
-
-let offsetHeight=document.querySelector('.header__topline').offsetHeight+document.querySelector('#slider').offsetHeight;
+let offsetHeight = document.querySelector('.header__topline').offsetHeight + document.querySelector('#slider').offsetHeight;
 
 
 function fixedNavbar(e) {
@@ -145,3 +208,8 @@ function fixedNavbar(e) {
 }
 
 window.addEventListener('scroll', fixedNavbar);
+
+document.querySelector('.login-link').addEventListener('click',(e)=>{
+    e.preventDefault();
+    BurgerApp.$modal.show('login');
+});
