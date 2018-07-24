be1a585aa0a1316bcc8446e39941b9bee23a8cc0
Тетерин Святослав <svyatoslavteterin@gmail.com>
2018-07-24 09:38:54 +0300

    vue cli fix

---
+++ app/src/main.js
@@ -0,0 +1,69 @@
+'use strict';
+
+
+window._ = require('lodash');
+
+window.Popper = require('popper.js')
+
+
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+// Subscribe to store updates
+store.subscribe((mutation, state) => {
+    // Store the state object as a JSON string
+    localStorage.setItem('store', JSON.stringify(state));
+});
+
+
+var VueCookie = require('vue-cookie');
+import VueTheMask from 'vue-the-mask';
+
+
+import VModal from 'vue-js-modal'
+
+
+import './registerServiceWorker'
+
+
+Vue.use(VueTheMask);
+
+Vue.use(VModal);
+Vue.use(VueCookie);
+
+
+import axios from "axios";
+
+Vue.prototype.$http = axios;
+
+
+Vue.config.productionTip = false;
+Vue.config.devtools = true;
+Vue.config.debug = true;
+
+new Vue({
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app')
+
+let offsetHeight = document.querySelector('.header__topline').offsetHeight;
+
+if (document.querySelector('#slider')) {
+    offsetHeight += document.querySelector('#slider').offsetHeight;
+}
+
+
+function fixedNavbar() {
+    if (document.querySelector('#mainnavbar')) {
+        if (window.pageYOffset >= offsetHeight) {
+            document.querySelector('#mainnavbar').classList.add("fixed");
+        } else {
+            document.querySelector('#mainnavbar').classList.remove("fixed");
+        }
+    }
+}
+
+window.addEventListener('scroll', fixedNavbar);
---
+++ app/src/views/Cart.vue
@@ -0,0 +1,16 @@
+<template>
+    <cart :items="cartItems"></cart>
+</template>
+
+<script>
+    // @ is an alias to /src
+    import cart from '@/components/cart.vue'
+
+    export default {
+        name: 'Cart',
+        components: {
+            cart
+        },
+        props: ["cartItems"]
+    }
+</script>
