1af746232d7d25966dded2d6cdb223947e329c07
Svyatoslav Teterin <you@example.com>
2018-07-31 19:08:25 +0300

    dishes item+dishes group Работает переключатель между категориями(макси, мини и т.п.)

---
+++ src/components/Dishes/item.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="btn" :class="{ active: active }" @click="setActiveDish(index)">{{dishData.dishName}}</div>
+</template>
+<script>
+  export default {
+    methods: {
+      setActiveDish(index) {
+        this.$emit('setActiveDish', index);
+      }
+    },
+    data() {
+      return {}
+    },
+    computed: {},
+    props: ["dishData", "active", "index"]
+  }
+</script>
--- src/components/newFood/item.vue
+++ src/components/newFood/item.vue
@@ -7,9 +7,9 @@
             <a class="i-add" @click.prevent="mods.include = !mods.include">добавить</a>
             <a class="i-more">развернуть</a>
 
-            <dishMods :mods="data.dishes[this.activeDish].ModGroups[1].mods"
-                      :dishId="this.data.dishes[this.activeDish].id"></dishMods>
-            <modGroups></modGroups>
+
+            <Dishes :dishes="this.data.dishes" :activeDish="this.activeDish"
+                    v-on:setActiveDish="setActiveDish"></Dishes>
             <div class="bottom">
                 <div class="price"><span v-text="getPrice"></span><span>₽</span></div>
                 <div class="weight"><span v-text="getWeight"></span> г.</div>
@@ -32,115 +32,116 @@
 </template>
 
 <script>
-    import dishMods from "@/components/newDishmod";
-    import dishModItem from "@/components/newDishmod/item.vue";
-    import amountControls from "@/components/amountControls";
-    import modGroups from "@/components/modGroups";
+  import dishMods from "@/components/newDishmod";
+  import dishModItem from "@/components/newDishmod/item.vue";
+  import amountControls from "@/components/amountControls";
+  import Dishes from "@/components/Dishes";
 
-    export default {
-        components: {dishMods, dishModItem, amountControls, modGroups},
-        methods: {
+  export default {
+    components: {dishMods, dishModItem, amountControls, Dishes},
+    methods: {
 
-            increment() {
-                this.$store.commit('addEquentity', {"value": this.data.dishes[this.activeDish]});
-            },
-            decrement() {
-                this.$store.commit('removeEquentity', {"value": this.data.dishes[this.activeDish]});
-            },
-            isActiveDish: function (index) {
-                if (index == this.activeDish) {
-                    return true;
-                }
-            },
-            addToCart: function () {
-              this.showCounter = !this.showCounter;
-
-                let dishData = {
-                    id: this.data.dishes[this.activeDish].id,
-                    dishName: this.data.dishes[this.activeDish].dishName,
-                    dishShortName: this.data.dishes[this.activeDish].dishShortName,
-                    dishExtName: this.data.ExternalName,
-                    price: this.data.dishes[this.activeDish].Price,
-                    outPrice: this.data.dishes[this.activeDish].OutPrice,
-                    sellType: "COUNT",
-                    mods: [],
-                    idShop: 3,
-                    position: this.data.ShowOrder,
-                    fullData: this.data.dishes[this.activeDish].fullData
-                };
-
-                this.$store.commit('addToCart', {'value': dishData});
-                alert('Добавлено');
-            },
-            setActiveDish: function (index) {
-                this.activeDish = index;
-            }
-        },
-        created() {
-            this.showCounter=!!this.count;
-        },
-        data() {
-            return {
-                activeDish: 0,
-                showCounter: false,
-                mods: {
-                    include: false,
-                    exclude: false
-                },
-            }
-        },
-
-        props: ['data'],
-
-        ready: function () {
-
-        },
-        mounted: function () {
-
-        },
-        computed: {
-            count: {
-                get: function () {
-                    let count = 0;
-                    const dish = this.$store.state.cart.find(p => p.id === this.data.dishes[this.activeDish].id);
-                    if (dish) {
-                        count = dish.count
-                    }
-                    return count;
-                },
-                set: function () {
-
-                }
-            },
-            dishData: function () {
-                return this.data.dishes[this.activeDish];
-            },
-            getPrice: function () {
-                let price = 0;
-                if (typeof this.data.dishes[this.activeDish] != "undefined") {
-                    price = this.data.dishes[this.activeDish].Price.slice(0, -3);
-                }
-                return price;
-
-            },
-            getWeight: function () {
-                if (typeof this.data.dishes[this.activeDish] != "undefined") {
-                    return this.data.dishes[this.activeDish].fullData.ExitMass;
-                }
-            },
-            getImage: function () {
-
-                // if (this.data.ImageName.indexOf('empty')>0){
-
-                //  }else{
-                // imageUrl='http://api.burger.jmedia.pro/public/images/'+this.data.id+'/430-275/'+this.data.ImageName;
-                //  }
-                return imageUrl;
-            },
-            getModWidth: function () {
-
-                return 100 / this.data.dishes.length;
-            }
+      increment() {
+        this.$store.commit('addEquentity', {"value": this.data.dishes[this.activeDish]});
+      },
+      decrement() {
+        this.$store.commit('removeEquentity', {"value": this.data.dishes[this.activeDish]});
+      },
+      isActiveDish: function (index) {
+        if (index == this.activeDish) {
+          return true;
         }
+      },
+      addToCart: function () {
+
+        let dishData = {
+          id: this.data.dishes[this.activeDish].id,
+          dishName: this.data.dishes[this.activeDish].dishName,
+          dishShortName: this.data.dishes[this.activeDish].dishShortName,
+          dishExtName: this.data.ExternalName,
+          price: this.data.dishes[this.activeDish].Price,
+          outPrice: this.data.dishes[this.activeDish].OutPrice,
+          sellType: "COUNT",
+          mods: [],
+          idShop: 3,
+          position: this.data.ShowOrder,
+          fullData: this.data.dishes[this.activeDish].fullData
+        };
+
+        this.$store.commit('addToCart', {'value': dishData});
+        alert('Добавлено');
+      },
+      setActiveDish: function (index) {
+        this.activeDish = index;
+      }
+    },
+    created() {
+      
+    },
+    data() {
+      return {
+        activeDish: 0,
+        mods: {
+          include: false,
+          exclude: false
+        },
+      }
+    },
+
+    props: ['data'],
+
+    ready: function () {
+
+    },
+    mounted: function () {
+
+    },
+    computed: {
+      showCounter:function() {
+        return this.count > 0 ? true : false;
+      },
+      count: {
+        get: function () {
+          let count = 0;
+          const dish = this.$store.state.cart.find(p => p.id === this.data.dishes[this.activeDish].id);
+          if (dish) {
+            count = dish.count
+          }
+          return count;
+        },
+        set: function () {
+
+        }
+      },
+      dishData: function () {
+        return this.data.dishes[this.activeDish];
+      },
+      getPrice: function () {
+        let price = 0;
+        if (typeof this.data.dishes[this.activeDish] != "undefined") {
+          price = this.data.dishes[this.activeDish].Price.slice(0, -3);
+        }
+        return price;
+
+      },
+      getWeight: function () {
+        if (typeof this.data.dishes[this.activeDish] != "undefined") {
+          return this.data.dishes[this.activeDish].fullData.ExitMass;
+        }
+      },
+      getImage: function () {
+
+        // if (this.data.ImageName.indexOf('empty')>0){
+
+        //  }else{
+        // imageUrl='http://api.burger.jmedia.pro/public/images/'+this.data.id+'/430-275/'+this.data.ImageName;
+        //  }
+        return imageUrl;
+      },
+      getModWidth: function () {
+
+        return 100 / this.data.dishes.length;
+      }
     }
+  }
 </script>
