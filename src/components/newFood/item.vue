7292fe909cefea4e3163a243b3b04874ac22138b
Тетерин Святослав <svyatoslavteterin@gmail.com>
2018-08-03 21:30:15 +0300

    ask deliveryInfo if we have not

--- src/components/newFood/item.vue
+++ src/components/newFood/item.vue
@@ -1,73 +1,73 @@
 <template>
-    <div :class="getClass" v-if="type==='offer'">
-        <div class="add-mo-to-cart">
-            <div class="mo-to-cart-img"></div>
-            <div class="mo-to-cart-text" v-show="!showCounter" @click="showCounter = !showCounter">Заказать</div>
-            <amountControls v-show="showCounter" :count="count" :showCounter="showCounter"
-                            v-on:decrement="decrement" v-on:increment="increment"></amountControls>
-        </div>
-        <div class="chars">
-            <div class="title">{{data.categName}}</div>
-            <div class="ingrids" v-if="data.dishes[this.activeDish].techCardData">
-                <dishMods :mods="data.dishes[this.activeDish].techCardData"
-                          :dishId="data.dishes[this.activeDish].id" :type="'exclude'"></dishMods>
-            </div>
-            <a class="i-add" @click.prevent="mods.include = !mods.include">добавить</a>
-            <a class="i-more" @click.prevent="mods.expanded = !mods.expanded">развернуть</a>
-            <div class="include-mods">
-
-                <dishMods v-if="mods.include" :mods="data.dishes[this.activeDish].ModGroups[1].mods"
-                          :dishId="data.dishes[this.activeDish].id"></dishMods>
-            </div>
-            <Dishes :dishes="this.data.dishes" v-if="this.data.dishes.length>1" :activeDish="this.activeDish"
-                    v-on:setActiveDish="setActiveDish"></Dishes>
-            <div class="bottom">
-                <div class="price"><span v-text="getPrice"></span><span>₽</span></div>
-                <div class="weight"><span v-text="getWeight"></span> г.</div>
-                <button class="in-basket" v-show="!showCounter" @click="addToCart">В корзину
-                </button>
-                <amountControls v-show="showCounter" :count="count" :showCounter="showCounter"
-                                v-on:decrement="decrement" v-on:increment="increment"></amountControls>
-            </div>
-        </div><!--chars-->
+  <div :class="getClass" v-if="type==='offer'">
+    <div class="add-mo-to-cart">
+      <div class="mo-to-cart-img"></div>
+      <div class="mo-to-cart-text" v-show="!showCounter" @click="showCounter = !showCounter">Заказать</div>
+      <amountControls v-show="showCounter" :count="count" :showCounter="showCounter"
+                      v-on:decrement="decrement" v-on:increment="increment"></amountControls>
     </div>
-    <div class="food" v-else>
-        <div class="chars">
-            <div class="top">
-                <div class="title">{{data.categName}}</div>
-                <div class="ingrids" v-if="data.dishes[this.activeDish].techCardData">
-                    <dishMods :mods="data.dishes[this.activeDish].techCardData"
-                              :dishId="data.dishes[this.activeDish].id" :type="'exclude'"></dishMods>
-                </div>
-                <a class="i-add" @click.prevent="mods.include = !mods.include"
-                   v-if="data.dishes[this.activeDish].ModGroups[1]">добавить</a>
-                <a class="i-more"
-                   v-if="data.dishes[this.activeDish].ModGroups[3]">развернуть</a>
-                <div class="include-mods">
-                    <dishMods v-if="mods.include" :mods="data.dishes[this.activeDish].ModGroups[1].mods"
-                              :dishId="data.dishes[this.activeDish].id" :type="'include'"></dishMods>
-                </div>
-                <Dishes :dishes="this.data.dishes" v-if="this.data.dishes.length>1" :activeDish="this.activeDish"
-                        v-on:setActiveDish="setActiveDish"></Dishes>
-            </div>
-            <div class="bottom">
-                <div class="price"><span v-text="getPrice"></span><span>₽</span></div>
-                <div class="weight"><span v-text="getWeight"></span> г.</div>
-                <button class="in-basket" v-show="!showCounter" @click="addToCart">В корзину</button>
+    <div class="chars">
+      <div class="title">{{data.categName}}</div>
+      <div class="ingrids" v-if="data.dishes[this.activeDish].techCardData">
+        <dishMods :mods="data.dishes[this.activeDish].techCardData"
+                  :dishId="data.dishes[this.activeDish].id" :type="'exclude'"></dishMods>
+      </div>
+      <a class="i-add" @click.prevent="mods.include = !mods.include">добавить</a>
+      <a class="i-more" @click.prevent="mods.expanded = !mods.expanded">развернуть</a>
+      <div class="include-mods">
 
-                <amountControls v-show="showCounter" :count="count" :showCounter="showCounter"
-                                v-on:decrement="decrement" v-on:increment="increment"></amountControls>
-            </div>
-        </div><!--chars-->
-        <div class="pic" @click="$modal.show('info')">
-            <div class="detail">
-                <div class="arrow"></div>
-                <div class="detail-img"></div>
-                <div class="detail-text">подробнее</div>
-            </div>
-            <img :src="getImage" alt="">
-        </div> <!--pic-->
-    </div> <!--food-->
+        <dishMods v-if="mods.include" :mods="data.dishes[this.activeDish].ModGroups[1].mods"
+                  :dishId="data.dishes[this.activeDish].id"></dishMods>
+      </div>
+      <Dishes :dishes="this.data.dishes" v-if="this.data.dishes.length>1" :activeDish="this.activeDish"
+              v-on:setActiveDish="setActiveDish"></Dishes>
+      <div class="bottom">
+        <div class="price"><span v-text="getPrice"></span><span>₽</span></div>
+        <div class="weight"><span v-text="getWeight"></span> г.</div>
+        <button class="in-basket" v-show="!showCounter" @click="addToCart">В корзину
+        </button>
+        <amountControls v-show="showCounter" :count="count" :showCounter="showCounter"
+                        v-on:decrement="decrement" v-on:increment="increment"></amountControls>
+      </div>
+    </div><!--chars-->
+  </div>
+  <div class="food" v-else>
+    <div class="chars">
+      <div class="top">
+        <div class="title">{{data.categName}}</div>
+        <div class="ingrids" v-if="data.dishes[this.activeDish].techCardData">
+          <dishMods :mods="data.dishes[this.activeDish].techCardData"
+                    :dishId="data.dishes[this.activeDish].id" :type="'exclude'"></dishMods>
+        </div>
+        <a class="i-add" @click.prevent="mods.include = !mods.include"
+           v-if="data.dishes[this.activeDish].ModGroups[1]">добавить</a>
+        <a class="i-more"
+           v-if="data.dishes[this.activeDish].ModGroups[3]">развернуть</a>
+        <div class="include-mods">
+          <dishMods v-if="mods.include" :mods="data.dishes[this.activeDish].ModGroups[1].mods"
+                    :dishId="data.dishes[this.activeDish].id" :type="'include'"></dishMods>
+        </div>
+        <Dishes :dishes="this.data.dishes" v-if="this.data.dishes.length>1" :activeDish="this.activeDish"
+                v-on:setActiveDish="setActiveDish"></Dishes>
+      </div>
+      <div class="bottom">
+        <div class="price"><span v-text="getPrice"></span><span>₽</span></div>
+        <div class="weight"><span v-text="getWeight"></span> г.</div>
+        <button class="in-basket" v-show="!showCounter" @click="addToCart">В корзину</button>
+
+        <amountControls v-show="showCounter" :count="count" :showCounter="showCounter"
+                        v-on:decrement="decrement" v-on:increment="increment"></amountControls>
+      </div>
+    </div><!--chars-->
+    <div class="pic" @click="$modal.show('info')">
+      <div class="detail">
+        <div class="arrow"></div>
+        <div class="detail-img"></div>
+        <div class="detail-text">подробнее</div>
+      </div>
+      <img :src="getImage" alt="">
+    </div> <!--pic-->
+  </div> <!--food-->
 </template>
 
 <script>
@@ -95,6 +95,7 @@
         }
       },
       addToCart: function () {
+
         let dishData = {
           id: this.data.dishes[this.activeDish].id,
           dishName: this.data.dishes[this.activeDish].dishName,
@@ -110,6 +111,11 @@
         };
 
         this.$store.commit("addToCart", {value: dishData});
+
+        if (!Object.keys(this.$store.state.deliveryInfo).length > 0) {
+
+          this.$modal.show('delivery');
+        }
       },
       setActiveDish: function (index) {
         this.activeDish = index;
