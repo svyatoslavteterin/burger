892f54b6f61601831d9cb8d34dd7e4608d3f9127
Тетерин Святослав <svyatoslavteterin@gmail.com>
2018-08-03 20:52:50 +0300

    forma

--- src/views/newHome/index.vue
+++ src/views/newHome/index.vue
@@ -1,54 +1,54 @@
 <template>
-    <div>
-        <MainOffer :menu="menu"></MainOffer>
-        <section id="mainnavbar">
-            <div class="container">
-                <div class="row v-middle">
-                    <div class="col-lg-1 col-xl-1 logo">
-                        <img src="@/assets/images/logo.svg" alt="logo" class="img-fluid"/>
-                    </div>
-                    <div class="col-lg-3 col-xl-3">
-                        <filters :filters="tags"></filters>
+  <div>
+    <MainOffer :menu="menu"></MainOffer>
+    <section id="mainnavbar">
+      <div class="container">
+        <div class="row v-middle">
+          <div class="col-lg-1 col-xl-1 logo">
+            <img src="@/assets/images/logo.svg" alt="logo" class="img-fluid"/>
+          </div>
+          <div class="col-lg-3 col-xl-3">
+            <filters :filters="tags"></filters>
 
-                        <search></search>
+            <search></search>
 
-                    </div>
-                    <div class="col-lg-8 col-xl-8  ">
-                        <MainMenu :data="menu"></MainMenu>
-                    </div>
-                </div>
+          </div>
+          <div class="col-lg-8 col-xl-8  ">
+            <MainMenu :data="menu"></MainMenu>
+          </div>
+        </div>
 
-            </div>
-        </section>
-        <section id="main">
-            <div class="container food">
-                <div class="row">
-                    <div class="container">
-                        <Foods :foods="foods"></Foods>
-                    </div>
-                </div>
-            </div>
-        </section>
-    </div>
+      </div>
+    </section>
+    <section id="main">
+      <div class="container food">
+        <div class="row">
+          <div class="container">
+            <Foods :foods="foods"></Foods>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-    // @ is an alias to /src
-    import MainMenu from "@/components/newMainMenu";
-    import filters from "@/components/newFilter";
-    import Foods from "@/components/newFood";
-    import MainOffer from "@/components/mainOffer";
-    import search from "@/components/newSearch";
+  // @ is an alias to /src
+  import MainMenu from "@/components/newMainMenu";
+  import filters from "@/components/newFilter";
+  import Foods from "@/components/newFood";
+  import MainOffer from "@/components/mainOffer";
+  import search from "@/components/newSearch";
 
-    export default {
-        name: "home",
-        components: {
-            MainOffer,
-            MainMenu,
-            Foods,
-            filters,
-            search
-        },
-        props: ["menu", "tags", "foods"]
-    };
+  export default {
+    name: "home",
+    components: {
+      MainOffer,
+      MainMenu,
+      Foods,
+      filters,
+      search
+    },
+    props: ["menu", "tags", "foods"]
+  };
 </script>
