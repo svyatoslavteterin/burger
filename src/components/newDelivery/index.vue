29ea8d052f748c2cb42637107c910f3dbd181b5c
Тетерин Святослав <svyatoslavteterin@gmail.com>
2018-08-13 11:42:32 +0300

    delivery info fixes

--- src/components/newDelivery/index.vue
+++ src/components/newDelivery/index.vue
@@ -32,66 +32,67 @@
         <input class="search-input" type="text" v-model="query" placeholder="Адрес доставки" required>
         <button class="search-btn"></button>
         <button class="clear-btn" @click="clearSearch"></button>
-        <ul class="result" v-if="!this.street">
+        <ul class="result" v-if="!this.deliveryInfo.Street">
           <li v-for="(street,index) in this.addresses" @click="setStreet(street.street)" :key="index"><span
             v-text="street.street"></span></li>
         </ul>
-        <ul class="result" v-if="this.street && !this.house">
-          <li v-for="(house,index) in this.addresses[this.street].houses"
-              :key="index"><span v-text="street+','+house.house" @click="setHouse(house.house)"></span></li>
+        <ul class="result" v-if="this.deliveryInfo.Street && !this.deliveryInfo.House">
+          <li v-for="(house,index) in this.addresses[this.deliveryInfo.Street].houses"
+              :key="index"><span v-text="deliveryInfo.Street+','+house.house" @click="setHouse(house.house)"></span>
+          </li>
         </ul>
 
       </div>
 
       <div class="small-inputs-wrapper">
         <div class="small-wrapper">
-          <input class="small-input lab-1" type="text" required>
+          <input class="small-input lab-1" type="text" required v-model="deliveryInfo.Entrance">
           <label for="">Подъезд</label>
         </div>
         <div class="small-wrapper">
-          <input class="small-input" type="text" required>
+          <input class="small-input" type="text" required v-model="deliveryInfo.Floor">
           <label for="">Этаж</label>
         </div>
         <div class="small-wrapper">
-          <input class="small-input" type="text" required>
+          <input class="small-input" type="text" required v-model="deliveryInfo.Apartment">
           <label for="">Квартира</label>
         </div>
         <div class="small-wrapper">
-          <input class="small-input" type="text" required>
+          <input class="small-input" type="text" required v-model="deliveryInfo.DoorphoneСode">
           <label for="">Домофон</label>
         </div>
       </div>
 
-      <div class="recieve-code-wrapper">
+      <div class="recieve-code-wrapper" v-if="!this.$store.getters.getAuthUser.id">
         <div class="auth-button">Авторизоваться</div>
-          <div class="">
-            <input type='checkbox' class='ios8-switch ios8-switch-lg' id='checkbox-3'>
-            <label for='checkbox-3'></label>
-          </div>
+        <div class="">
+          <input type='checkbox' class='ios8-switch ios8-switch-lg' id='checkbox-3'>
+          <label for='checkbox-3'></label>
+        </div>
         <div class="reg-button">Зарегистрироваться</div>
       </div>
-      <div class="recieve-code-wrapper">
+      <div class="recieve-code-wrapper" v-if="!this.$store.getters.getAuthUser.id">
         <div class="small-wrapper phone-wrapper">
-          <input class="small-input2" 
-          type="text" 
-          placeholder="Номер телефона в любом формате" 
-          required>
+          <input class="small-input2"
+                 type="text"
+                 placeholder="Номер телефона в любом формате"
+                 required>
         </div>
-        <div class="small-wrapper phone-wrapper">
+        <div class="small-wrapper phone-wrapper" v-if="!this.$store.getters.getAuthUser.id">
           <input
-            class="small-input2-error" 
+            class="small-input2-error"
             type="password"
             placeholder="Пароль"
-          required>
+            required>
           <img src="./img/icons_incorrect.svg" class="icons_incorrect">
         </div>
       </div>
-      <div class="recieve-code-wrapper" style="justify-content: flex-end;">
+      <div class="recieve-code-wrapper" style="justify-content: flex-end;" v-if="!this.$store.getters.getAuthUser.id">
         <div class="error-mess">Неверный пароль</div>
         <div class="send-again-link">Напомнить?</div>
       </div>
       <div class="recieve-code-wrapper" style="justify-content: center;">
-        <div class="send-button">Отправить</div>
+        <div class="send-button" @click="saveDelivery">Сохранить</div>
       </div>
 
       <div v-if="okDelivery" class="thank-you-message">
@@ -171,115 +172,129 @@
   </modal>
 </template>
 <script>
-import VuePerfectScrollbar from "vue-perfect-scrollbar";
-import modalActions from "@/mixins/modalActions";
-import "./style.scss";
+  import VuePerfectScrollbar from "vue-perfect-scrollbar";
+  import modalActions from "@/mixins/modalActions";
+  import "./style.scss";
 
-export default {
-  components: { VuePerfectScrollbar },
-  mixins: [modalActions],
-  data() {
-    return {
-      okButton: false,
-      okDelivery: false,
-      okSelfDelivery: false,
-      activeTab: 1,
-      addresses: [],
-      houses: [],
-      house: "",
-      q: "",
-      address: "",
-      street: ""
-    };
-  },
-  methods: {
-    checkAddress: function(e) {
-      let container = e.target.parentNode;
+  export default {
+    components: {VuePerfectScrollbar},
+    mixins: [modalActions],
+    data() {
+      return {
+        okButton: false,
+        okDelivery: false,
+        okSelfDelivery: false,
+        deliveryInfo: {
+          "Street": '',
+          "House": '',
+          "Housing": '',
+          "Structure": '',
+          "Office": '',
+          "Entrance": '',
+          "DoorphoneСode": '',
+          "Floor": '',
+          "Apartment": ''
+        },
+        activeTab: 1,
+        addresses: [],
+        houses: [],
+        house: "",
+        q: "",
+        address: "",
+        street: ""
+      };
+    },
+    methods: {
+      saveDelivery(){
+        this.$store.commit("setDeliveryInfo", this.deliveryInfo);
+        this.$modal.hide('delivery');
+      },
+      checkAddress: function (e) {
+        let container = e.target.parentNode;
 
-      let addressValue = container.querySelector(".address-value").innerText;
-      this.address = addressValue;
-    },
-    clearSearch() {
-      this.q = "";
-    },
-    setStreet(street) {
-      this.street = street;
-    },
-    setHouse(house) {
-      this.house = house;
-    },
-    getAdresses: async function() {
-      try {
-        let response = await this.$http.get(
-          "https://apitest.burgerpizzoni.ru/api/Address/get?street=" +
+        let addressValue = container.querySelector(".address-value").innerText;
+        this.address = addressValue;
+      },
+      clearSearch() {
+        this.q = "";
+      },
+      setStreet(street) {
+        this.deliveryInfo.Street = street;
+      },
+      setHouse(house) {
+        this.deliveryInfo.House = house;
+      },
+      getAdresses: async function () {
+        try {
+          let response = await this.$http.get(
+            "https://apitest.burgerpizzoni.ru/api/Address/get?street=" +
             this.q +
             "&access_token=" +
             this.$store.state.authUser.id
-        );
-        return response.data;
-      } catch (e) {
-        this.errors.address.request = "Ошибка при получении адресов";
-      }
-    },
-    showAddresses() {
-      this.getAdresses(this.q).then(addresses => {
-        if (typeof addresses != "undefined") {
-          this.addresses = addresses;
-        }
-      });
-    }
-  },
-  computed: {
-    fullAddress: function() {
-      return this.street + "," + this.house;
-    },
-    query: {
-      set: function(newValue) {
-        this.showAddresses();
-        this.q = newValue;
-        if (this.street && this.house) {
-          let addressArr = this.q.split(",");
-          let house = addressArr[1];
-          let street = addressArr[0];
-
-          if (this.addresses.indexOf(street) < 0) {
-            this.street = "";
-            this.house = "";
-          } else {
-            if (this.addresses[street].houses.indexOf(house) < 0) {
-              this.house = "";
-            }
-          }
-          this.$store.commit("setDeliveryInfo", {
-            street: this.street,
-            house: this.house
-          });
-        } else {
-          if (this.street) {
-            if (this.addresses.indexOf(this.street) < 0) {
-              this.street = "";
-              this.house = "";
-            }
-          }
+          );
+          return response.data;
+        } catch (e) {
+          this.errors.address.request = "Ошибка при получении адресов";
         }
       },
-      get: function() {
-        if (this.street) {
-          if (this.house) {
-            this.$store.commit("setDeliveryInfo", {
-              street: this.street,
-              house: this.house
-            });
-            return this.fullAddress;
-          } else {
-            return this.street;
+      showAddresses() {
+        this.getAdresses(this.q).then(addresses => {
+          if (typeof addresses != "undefined") {
+            this.addresses = addresses;
           }
-        } else {
-          return this.q;
+        });
+      }
+    },
+    computed: {
+      fullAddress: function () {
+        return this.deliveryInfo.Street + "," + this.deliveryInfo.House;
+      },
+      query: {
+        set: function (newValue) {
+          this.showAddresses();
+          this.q = newValue;
+
+          if (this.deliveryInfo.Street && this.deliveryInfo.House) {
+            let addressArr = this.q.split(",");
+            let house = addressArr[1];
+            let street = addressArr[0];
+
+            if (!street) {
+              this.deliveryInfo={};
+              return;
+            }
+            if (this.addresses[street]) {
+              this.deliveryInfo.House = '';
+            } else {
+              if (this.addresses[street].houses.indexOf(house) < 0) {
+                this.deliveryInfo.House = '';
+              }
+            }
+          } else {
+            if (this.deliveryInfo.Street) {
+              if (this.addresses.indexOf(this.deliveryInfo.Street) < 0) {
+                this.deliveryInfo.Street = '';
+                this.deliveryInfo.House = '';
+              }
+            } else {
+              this.deliveryInfo.Street = '';
+              this.deliveryInfo.House = '';
+            }
+          }
+        },
+        get: function () {
+          if (this.deliveryInfo.Street) {
+            if (this.deliveryInfo.House) {
+              return this.fullAddress;
+            } else {
+              return this.deliveryInfo.Street;
+            }
+          } else {
+            return this.q;
+          }
         }
       }
     }
-  }
-};
+  };
 </script>
 
